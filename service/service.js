fb171e9ea4e15190c8b22fecd2f3187dee6f99ed
XIE <3217454073@qq.com>
2023-06-21 14:30:31 +0800

    解决https无法访问的问题

--- service/service.js
+++ service/service.js
@@ -11,10 +11,39 @@ const app = express();
 app.use(express.json());	//中间件，解析表单中的 JSON 格式的数据
 app.use(express.urlencoded({ extended: true }));	//解析 URL-encoded 格式的请求体数据
 
+
+/**
+ * 使用http协议 ，请注意使用https就将此段代码注释
+ */
+
+
 const http = require('http');
 const server = http.Server(app);		// 用http模块创建一个服务并把express的实例挂载上去
-
 const io = require('socket.io')(server, { cors: true });	// 引入socket.io并立即实例化，把server挂载上去
+//端口启动
+server.listen(3030, function () {
+    console.log('【轮子哥】服务端启动成功,端口[3030]');
+});
+
+
+/**
+ * 使用https，请注意使用https的话就将此段代码取消注释
+ */
+
+// const fs = require('fs');
+// const options = {
+//   key: fs.readFileSync('./ctrlc.cc.key'),
+//   cert: fs.readFileSync('./ctrlc.cc.pem'),
+// };
+// var https = require('https').Server(options, app);
+// var io = require('socket.io')(https,{cors:true});
+// //端口启动
+// https.listen(3030, function () {
+//     console.log('【轮子哥】服务端启动成功,端口[3030]');
+// });
+
+
+
 
 const cors = require("cors");
 app.use(cors()); //使用cors中间件解决跨域问题
@@ -33,7 +62,6 @@ app.use(cors()); //使用cors中间件解决跨域问题
 //     else
 //         next();
 // });
-
 
 
 //记录所有加入的用户
@@ -526,8 +554,3 @@ app.post('/commentReply', function (req, res) {
         res.json(statu.filter((v) => v.type == "dataFalse"))
     }
 })
-
-//端口启动
-server.listen(3030, function () {
-    console.log('【轮子哥】服务端启动成功,端口[3030]');
-});
